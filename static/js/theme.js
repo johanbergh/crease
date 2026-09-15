e71b6d53296c9d271d4ce01369ea672a662f05e3
Optimal7 <mrscatman7@gmail.com>
2026-09-15 11:35:04 +0200

    complete crease rework

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,43 +1,25 @@
-// Variables (to make it 10x easier)
-var element = document.body;
-var x = document.getElementById("icon");
+// Theme toggle (stored in localStorage)
+(function () {
+    const root = document.documentElement;
+    const iconEl = document.getElementById("theme-icon");
 
-// Checks Localstorage if prefence is saved
-if (localStorage.getItem('color-theme') === 'dark') {
-    element.className = 'dark-mode';
-    x.innerHTML = '<i class="fa-solid fa-moon fa-xl"></i>'
-
-    // White
-} else if (localStorage.getItem('color-theme') === 'white') {
-    element.className = '#';
-    x.innerHTML = '<i class="fa-solid fa-sun fa-xl"></i>'
-
-    // If no localstorage prefrence is saved
-} else {
-    localStorage.setItem('color-theme', 'white')
-    element.className = '#';
-    x.innerHTML = '<i class="fa-solid fa-sun fa-xl"></i>'
-}
-
-
-
-function color_mode() {
-    // Gets Localstorage value
-    theme = localStorage.getItem('color-theme')
-
-    // Changes CSS & Icons accordingly
-    if (theme === 'white') {
-        localStorage.setItem('color-theme', 'dark')
-        element.className = 'dark-mode';
-        x.innerHTML = '<i class="fa-solid fa-moon fa-xl"></i>'
-
-    } else if (theme === 'dark') {
-        localStorage.setItem('color-theme', 'white')
-        element.className = '#';
-        x.innerHTML = '<i class="fa-solid fa-sun fa-xl"></i>'
+    function applyTheme(theme) {
+        if (theme === "dark") {
+            root.setAttribute("data-theme", "dark");
+            if (iconEl) iconEl.className = "fa-solid fa-sun fa-lg";
+        } else {
+            root.removeAttribute("data-theme");
+            if (iconEl) iconEl.className = "fa-solid fa-moon fa-lg";
+        }
     }
 
+    const saved = localStorage.getItem("crease-theme") || "light";
+    applyTheme(saved);
 
-
-
-}
+    window.toggleTheme = function () {
+        const current = localStorage.getItem("crease-theme") || "light";
+        const next = current === "dark" ? "light" : "dark";
+        localStorage.setItem("crease-theme", next);
+        applyTheme(next);
+    };
+})();
